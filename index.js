495e4fdd4972740b3a06e6f4e9926977e5289688
Lakpa Dietz <ldietz086@gmail.com>
2022-11-17 00:22:22 -0800

    Enabled cors support and added post

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 require("dotenv").config();
-const BACK_END = process.env.PORT;
 
 const express = require("express");
-const path = require("node:path");
 const app = express();
 
+const cors = require("cors");
 app.get("/", (_req, res) => {
   res.sendFile(__dirname + "/index.html");
-});
-
-app.listen(BACK_END || 5500, () => {
-  console.log(`The server is running on ${BACK_END}! You better go catch it`);
 });
 
 const videoRouter = require('./routes/videos.js');
 
 app.use('/videos', videoRouter);
+app.use(cors());
+
+const BACK_END = process.env.PORT;
+app.listen(BACK_END || 5500, () => {
+  console.log(`The server is running on ${BACK_END}! You better go catch it`);
+});
--- routes/videos.js
+++ routes/videos.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("node:path");
-const app = express();
-
 const router = express.Router();
 
-const videos = path.join(__dirname, "../data/videos.json");
+const videosJSONFile = path.join(__dirname, "../data/videos.json");
+const videos = require(videosJSONFile);
 
 router.get("/", (_req, res) => {
   try {
@@ -14,7 +13,7 @@ router.get("/", (_req, res) => {
   }
 });
 
-router.get("/:videoId", (_req, res) => {
+router.get("/:videoId", (req, res) => {
   const found = videos.find((video) => video.id === req.params.videoId);
 
   if (found) {
@@ -23,5 +22,31 @@ router.get("/:videoId", (_req, res) => {
     res.status(404).json({ error: `Video with ID ${req.params.videoId} not found` });
   }
 });
+
+router.post("/post", (req,res) => {
+    const {title, description} = req.body;
+    if (!title || !description) {
+        return res.status(400).json({ error: "Please provide a title and description"});
+    }
+
+    const newVideo = {
+      id: "c05b9a93-8682-4ab6-aff2-92ebb4bbfc14",
+      title: title,
+      channel: "Christmas",
+      image: "../public/images/christmas-kitty.jpg",
+      description: description,
+      views: "0",
+      likes: "0",
+      duration: "50:00",
+      video: "https://project-2-api.herokuapp.com/stream",
+      timestamp: 1668508367,
+    };
+
+    videos.push(newVideo);
+    writeJSONFile(videosJSONFile, videos);
+
+    res.status(201).json(newVideo)
+});
+
 
 module.exports = router;
