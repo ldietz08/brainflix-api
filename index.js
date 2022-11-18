1153facf136bb78149c6197d058c9652ee3a1a5e
Lakpa Dietz <ldietz086@gmail.com>
2022-11-18 13:07:49 -0800

    Added a helper file and fixed image paths

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ app.get("/", (_req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-const videoRouter = require('./routes/videos.js');
+const videoRouter = require("./routes/videos.js");
 
-app.use('/videos', videoRouter);
+app.use("/videos", videoRouter);
 app.use(cors());
 
-const BACK_END = process.env.PORT;
-app.listen(BACK_END || 5500, () => {
-  console.log(`The server is running on ${BACK_END}! You better go catch it`);
+app.use(express.static('public'));
+app.use('/images', express.static('images'));
+
+
+const BACK_END_URL = process.env.PORT;
+app.listen(BACK_END_URL || 5500, () => {
+  console.log(`The server is running on ${BACK_END_URL}! You better go catch it`);
 });
--- routes/videos.js
+++ routes/videos.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const path = require("node:path");
 const router = express.Router();
+const cors = require("cors");
 
+router.use(express.json());
 const videosJSONFile = path.join(__dirname, "../data/videos.json");
 const videos = require(videosJSONFile);
+
+const { getNewId, writeJSONFile } = require("../helper/helper");
+
+router.use(cors());
 
 router.get("/", (_req, res) => {
   try {
@@ -13,40 +19,45 @@ router.get("/", (_req, res) => {
   }
 });
 
+// Read
 router.get("/:videoId", (req, res) => {
   const found = videos.find((video) => video.id === req.params.videoId);
 
   if (found) {
     res.status(200).json(found);
   } else {
-    res.status(404).json({ error: `Video with ID ${req.params.videoId} not found` });
+    res
+      .status(404)
+      .json({ error: `Video with ID ${req.params.videoId} not found` });
   }
 });
 
-router.post("/post", (req,res) => {
-    const {title, description} = req.body;
-    if (!title || !description) {
-        return res.status(400).json({ error: "Please provide a title and description"});
-    }
+// Create new video
+router.post("/", (req, res) => {
+  const { title, description } = req.body;
+  if (!title || !description) {
+    return res
+      .status(400)
+      .json({ error: "Please provide a title and description" });
+  }
 
-    const newVideo = {
-      id: "c05b9a93-8682-4ab6-aff2-92ebb4bbfc14",
-      title: title,
-      channel: "Christmas",
-      image: "../public/images/christmas-kitty.jpg",
-      description: description,
-      views: "0",
-      likes: "0",
-      duration: "50:00",
-      video: "https://project-2-api.herokuapp.com/stream",
-      timestamp: 1668508367,
-    };
+  const newVideo = {
+    id: getNewId(),
+    title: title,
+    channel: "Christmas Kitty",
+    image: "http://localhost:8080/images/christmas-kitty.jpg",
+    description: description,
+    views: "0",
+    likes: "0",
+    duration: "50:00",
+    video: "https://project-2-api.herokuapp.com/stream",
+    timestamp: 1668508367,
+  };
 
-    videos.push(newVideo);
-    writeJSONFile(videosJSONFile, videos);
+  videos.push(newVideo);
+  writeJSONFile(videosJSONFile, videos);
 
-    res.status(201).json(newVideo)
+  res.status(201).json(newVideo);
 });
-
 
 module.exports = router;
